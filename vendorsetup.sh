0a1aacf352a9f0b1d73c4c65970fefdfd1b8af91
android <trevd1234@gmail.com>
2014-01-03 09:37:30 +0000

    update readme and add embedded superuser settings patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,16 +22,19 @@ function copy_manifests(){
 		echo "Not on the AOSP Master Branch.... Skipping Archos setup"
 	else	
 	
-		echo "Patching Chromium"
-		patch -p1 --silent --forward --reject-file=- --strip=1 --directory=external/chromium_org < vendor/archos/a80sboard/patches/chromium.patch
-		echo "Patching Native Framework"
-		patch  --silent --forward --reject-file=- --strip=0 < vendor/archos/a80sboard/patches/frameworks_native.patch
-		echo "Patching Libhardware Framework"
-		patch  --silent --forward --reject-file=- --strip=0 < vendor/archos/a80sboard/patches/libhardware.patch
-		echo "Patching System Core"
-		patch  --silent --forward --reject-file=- --strip=0 < vendor/archos/a80sboard/patches/system_core.patch
-		echo "Patching Hackers Keyboard"
-		patch  --silent --forward --reject-file=- --strip=0 < vendor/archos/a80sboard/patches/hackerskeyboard.patch
+    echo "Patching Chromium"
+    patch -p1 --silent --forward --reject-file=- --strip=1 --directory=external/chromium_org < vendor/archos/a80sboard/patches/chromium.patch
+    echo "Patching Native Framework"
+    patch  --silent --forward --reject-file=- --strip=0 < vendor/archos/a80sboard/patches/frameworks_native.patch
+    echo "Patching Libhardware Framework"
+    patch  --silent --forward --reject-file=- --strip=0 < vendor/archos/a80sboard/patches/libhardware.patch
+    echo "Patching System Core"
+    patch  --silent --forward --reject-file=- --strip=0 < vendor/archos/a80sboard/patches/system_core.patch
+    echo "Patching Hackers Keyboard"
+    patch  --silent --forward --reject-file=- --strip=0 < vendor/archos/a80sboard/patches/hackerskeyboard.patch
+    echo "Patching Settings to enable embedded superuser"
+    patch  --silent --forward --reject-file=- --strip=0 < vendor/archos/a80sboard/patches/embedded_superuser.patch
+
 		
 		copy_manifests
 		rm -rf system/extra/su
