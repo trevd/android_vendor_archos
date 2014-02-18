aa652eca6640f6d7394288984d6ede73ccb9d469
android <trevd1234@gmail.com>
2014-02-18 23:40:03 +0000

    Custom build systems definitions to add PRODUCT_PACKAGES filtering without having to define a completely new product_config or create unnecessary forks of upstream repos
    Example :
    
    Remove OpenWnn and associated libs by appending the following
    the end of a device mk file , e.g device/archos/a80sboard/full_a80sboard.mk
    
    PRODUCT_PACKAGES_FILTER := \
            OpenWnn \
            PinyinIME \
            libWnnEngDic \
            libWnnJpnDic \
            libwnndict \

---
+++ build/core/definitions.mk
@@ -0,0 +1,34 @@
+# 
+# Copyright (C) 2014 Trevor Drake
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+##
+## Custom Build System Definitons
+## Called from build/core/definitions.mk
+## Use this file to 
+## 1. Add additonal global helper functions 
+## 2. Override existing functions defined by definitions.mk
+##
+
+## Stash the old expand-required-modules recipe before
+## defining a new one.
+expand-required-modules-old=$(value expand-required-modules-old)
+
+## The new expand-required-modules filters out any
+## Packages defined by PRODUCT_PACKAGES_FILTER
+define expand-required-modules
+$(call _expand-required-modules-old,$(1),$(2))\
+$(eval $(1) := $(filter-out $(PRODUCT_PACKAGES_FILTER),$($(1))))
+endef
